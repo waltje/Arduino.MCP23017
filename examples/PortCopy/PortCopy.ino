8f2721036b5e82f9b49b172a5af69f5182207ff8
Fred N. van Kempen <decwiz@yahoo.com>
2025-09-03 02:55:30 -0400

    Updated the examples.

--- examples/PortCopy/PortCopy.ino
+++ examples/PortCopy/PortCopy.ino
@@ -4,35 +4,47 @@
  * Use active low inputs on port B. Internal pullups are enabled by default by the library so there is no need for external resistors.
  * Place LEDS on port A for instance. 
  * When pressing a button, the corresponding led is shut down.
- * 
+ *
  * You can also uncomment one line to invert the input (when pressing a button the corresponding led is lit)
  */
 #include <Wire.h>
 #include <MCP23017.h>
 
 #define MCP23017_ADDR 0x20
-MCP23017 mcp = MCP23017(MCP23017_ADDR);
 
-void setup() {
-    Wire.begin();
-    Serial.begin(115200);
-    
-    mcp.init();
-    mcp.portMode(MCP23017Port::A, 0);          //Port A as output
-    mcp.portMode(MCP23017Port::B, 0b11111111); //Port B as input
+MCP23017 mcp(MCP23017_ADDR);
 
-    mcp.writeRegister(MCP23017Register::GPIO_A, 0x00);  //Reset port A 
-    mcp.writeRegister(MCP23017Register::GPIO_B, 0x00);  //Reset port B
 
-    // GPIO_B reflects the same logic as the input pins state
-    mcp.writeRegister(MCP23017Register::IPOL_B, 0x00);
-    // Uncomment this line to invert inputs (press a button to lit a led)
-    //mcp.writeRegister(MCP23017Register::IPOL_B, 0xFF);
+void setup()
+{
+  uint32_t tmr = millis();
+
+  Serial.begin(115200);
+  while (!Serial && ((millis() - tmr) < 5000))
+        ;
+  Serial.println();
+
+  Wire.begin();
+
+  mcp.init();
+  mcp.portMode(MCP23017Port::A, 0);          //Port A as output
+  mcp.portMode(MCP23017Port::B, 0b11111111); //Port B as input
+
+  mcp.writeRegister(MCP23017Register::GPIO_A, 0x00);  //Reset port A
+  mcp.writeRegister(MCP23017Register::GPIO_B, 0x00);  //Reset port B
+
+  // GPIO_B reflects the same logic as the input pins state
+  mcp.writeRegister(MCP23017Register::IPOL_B, 0x00);
+
+  // Uncomment this line to invert inputs (press a button to lit a led)
+  //mcp.writeRegister(MCP23017Register::IPOL_B, 0xFF);
 }
 
-void loop() {
-    uint8_t currentB;
 
-    currentB = mcp.readPort(MCP23017Port::B);
-    mcp.writePort(MCP23017Port::A, currentB);
+void loop()
+{
+  uint8_t currentB;
+
+  currentB = mcp.readPort(MCP23017Port::B);
+  mcp.writePort(MCP23017Port::A, currentB);
 }
